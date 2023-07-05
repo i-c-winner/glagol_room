02163144bf6679c826d6a917a1aa447401ca27fc
Delousov Dmitiy <dmitriy@pop-os.localdomain>
2023-07-05 17:39:52 +0500

    feat: добавить стили
    * Создать кастомные темы

--- src/components/createrRoomComponent/CreateRoomComponent.tsx
+++ src/components/createrRoomComponent/CreateRoomComponent.tsx
@@ -1,7 +1,28 @@
 import { useEffect, useState } from 'react'
 import getRandomText from "../../plugins/getRandomText"
+import { TextField } from "@mui/material"
+import { Box } from "@mui/material"
+import theme from "../../Ui/themes/theme"
+
 
 export default function createRoomComponent(props: any) {
+	console.log(theme);
+	const stylesInput={
+		backgroundColor: theme.palette.background.paper,
+		margin: '0 auto',
+		width: '25%'
+	}
+	const stylesBox={
+		backgroundColor: 'grey',
+		margin: 0,
+		position: "absolute",
+		top: '0px',
+		left: '0px',
+		right: '0px',
+		bottom: '0px',
+		paddingTop: '25vh',
+		textAlign: 'center'
+	}
 	const [room, setRoom]=useState(window.location.pathname.split('/')[1])
 	useEffect(() => {
 		if (room!=="") {
@@ -12,6 +33,12 @@ export default function createRoomComponent(props: any) {
 		props.action()
 	}
 	return (
-		<div onClick={switcher}>createRoomComponent</div>
+		<Box
+			sx={stylesBox}
+		>
+			<TextField
+				sx={stylesInput}
+				id="outlined-basic" label="Outlined" variant="outlined" />
+		</Box>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "@emotion/react"
 import route from "./route/route"
-
+import theme from "./Ui/themes/theme"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<RouterProvider router={route} />
+	<ThemeProvider theme={theme}>
+		<RouterProvider router={route} ></RouterProvider>
+	</ThemeProvider>
 )
